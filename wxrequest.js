9ffadf5777c4e5e5d6c8f78f590b517764464777
heyang <hetianxiao90@163.com>
2019-01-17 11:56:28 +0800

    添加状态

--- wxrequest.js
+++ wxrequest.js
@@ -32,3 +32,27 @@ const RequestHandle = (options={}) => {
         }) 
     })
 }
+
+const RequestError = function(data){
+    switch(data.statusCode){
+        case 401:
+            wx.showToast({title:'登陆过期，即将重新登陆！',icon:'none'});
+            setTimeout(()=>{
+                wx.redirectTo({
+                    url:"/pages/home/start",
+                });
+            },1500)
+            
+            break;
+        case 405:
+            wx.showToast({title:'小程序错误，请重新打开小程序！',icon:'none'});
+            // console.log('请求方式错误');
+            break;
+        default:
+            wx.redirectTo({
+                url:"/pages/home/start",
+            });
+            break;
+    }
+}
+exports.RequestHandle = RequestHandle;
